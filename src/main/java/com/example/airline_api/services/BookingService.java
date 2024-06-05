9632e3015f58888c4cf9edd07f14c7ca9c564891
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-06-05 17:42:41 +0100

    added repositories, services, dataloader and columns to models

---
+++ src/main/java/com/example/airline_api/services/BookingService.java
@@ -0,0 +1,12 @@
+package com.example.airline_api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+    @Autowired
+
+
+}
