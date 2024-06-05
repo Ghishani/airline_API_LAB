25d36795f02245319424df097dfdffe488dfdd60
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-06-05 17:58:53 +0100

    created BookingDTO

--- src/main/java/com/example/airline_api/services/BookingService.java
+++ src/main/java/com/example/airline_api/services/BookingService.java
@@ -1,5 +1,9 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Booking;
+import com.example.airline_api.models.BookingDTO;
+import com.example.airline_api.repositories.BookingRepository;
+import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +11,17 @@ import org.springframework.stereotype.Service;
 public class BookingService {
 
     @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    PassengerService passengerService;
+
+    @Autowired
+    FlightService flightService;
+
+    public Booking saveBooking(BookingDTO bookingDTO){
+
+    }
 
 
 }
