33e336dbbd5beb73eebae5673156e5e3c0ffa85d
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-06-06 08:59:20 +0100

    created controllers

--- src/main/java/com/example/airline_api/services/FlightService.java
+++ src/main/java/com/example/airline_api/services/FlightService.java
@@ -1,6 +1,8 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Booking;
 import com.example.airline_api.models.Flight;
+import com.example.airline_api.repositories.BookingRepository;
 import com.example.airline_api.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,9 @@ public class FlightService {
     @Autowired
     FlightRepository flightRepository;
 
+    @Autowired
+    BookingRepository bookingRepository;
+
     public void saveFlight(Flight flight){
         flightRepository.save(flight);
     }
@@ -24,5 +29,13 @@ public class FlightService {
 
     public Optional<Flight> findSingleFlight(Long id){
         return flightRepository.findById(id);
+    }
+
+    public void deleteFlight(long flightId){ // ADDED
+        Flight flight = findSingleFlight(flightId).get();
+        for (Booking booking : flight.getBookings()){
+            bookingRepository.deleteById(booking.getId());
+        }
+        flightRepository.deleteById(flightId);
     }
 }
